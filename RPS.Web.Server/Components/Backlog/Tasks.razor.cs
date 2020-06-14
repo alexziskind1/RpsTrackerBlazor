617f9ea80fe44f44e0ea986ee061096512ce3c7a
alexziskind1 <alex@nuvious.com>
2020-06-14 08:24:37 -0400

    tasks

---
+++ RPS.Web.Server/Components/Backlog/Tasks.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using RPS.Core.Models;
+using RPS.Core.Models.Dto;
+using RPS.Data;
+using RPS.Web.Server.Models.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPS.Web.Server.Components.Backlog
+{
+    public partial class Tasks : ComponentBase
+    {
+
+        [Parameter]
+        public PtItem Item { get; set; }
+
+        // public TasksFormModel Model = new TasksFormModel();
+
+        public List<PtTask> TaskItems = new List<PtTask>();
+
+        public string NewTaskTitle = string.Empty;
+
+        [Inject]
+        private IPtTasksRepository RpsTasksRepo { get; set; }
+
+        protected override void OnInitialized()
+        {
+            TaskItems = Item.Tasks;
+            base.OnInitialized();
+        }
+
+        public void AddTaskHandler()
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskTitle))
+            {
+                // TaskItems.Add(new PtTask { Title = NewTaskTitle });
+                SaveTask();
+               ///TaskItems.Add(createdTask);
+                NewTaskTitle = string.Empty;
+            }
+        }
+
+        private void SaveTask()
+        {
+            PtNewTask taskNew = new PtNewTask
+            {
+                ItemId = Item.Id,
+                Title = NewTaskTitle
+            };
+
+             RpsTasksRepo.AddNewTask(taskNew);
+        }
+
+
+    }
+}
--- RPS.Web.Server/Models/Forms/TasksFormModel.cs
+++ RPS.Web.Server/Models/Forms/TasksFormModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPS.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace RPS.Web.Server.Models.Forms
 {
     public class TasksFormModel
     {
+
+        public int ItemId { get; set; }
+        public string NewTaskTitle { get; set; }
+
+        public List<PtTask> Tasks = new List<PtTask>();
+
+
     }
 }
